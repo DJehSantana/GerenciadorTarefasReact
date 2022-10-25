9d42b42b4fd3c6a25ed325fc8893bd7289984831
DJehSantana <jessicah.dias23@hotmail.com>
2022-10-25 19:40:38 -0300

    criando componente item tarefa

--- src/componentes/Item.js
+++ src/componentes/Item.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import naoConcluido from '../assets/icones/notChecked.svg';
+import concluido from '../assets/icones/checked.svg';
 
 export const Item = props => {
 
-    const {tarefa} = props;
+    const { tarefa } = props;
 
     return (
         <div className="container-item">
-            <p>{tarefa.nome}</p>
+            <img src= {naoConcluido} alt= "Selecionar tarefa" />
+            <div>
+                <p>{tarefa?.nome}</p>
+                <p>Previsão de conclusão em: {tarefa?.dataPrevistaConclusao}</p>
+            </div>
+
         </div>
     )
 }
