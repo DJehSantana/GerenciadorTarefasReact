0d0ca12392034fd2c48f4b2a9654380140ca7bb5
DJehSantana <jessicah.dias23@hotmail.com>
2022-10-31 09:35:20 -0300

    corrigindo bug

--- src/componentes/Listagem.js
+++ src/componentes/Listagem.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useState } from 'react';
 import { Modal } from 'react-bootstrap';
 import moment from 'moment';
 import imgVazio from '../assets/icones/imgvazio.svg';
@@ -12,18 +13,18 @@ export const Listagem = props => {
 
     //states do modal
     const [showModal, setShowModal] = useState(false);
+    const [msgErro, setMsgErro] = useState('');
     const [idTarefa, setIdTarefa] = useState(null);
     const [nomeTarefa, setNomeTarefa] = useState('');
     const [dataConclusao, setDataConclusao] = useState('');
     const [dataPrevistaConclusao, setDataPrevistaConclusao] = useState('');
-    const [msgErro, setMsgErro] = useState('');
 
     //função que ativa edição da tarefa
     const selecionarTarefa = tarefa => {
         setIdTarefa(tarefa.id);
         setNomeTarefa(tarefa.nome);
-        setDataPrevistaConclusao(moment(tarefa.dataPrevistaConclusao).format('yyyy MM DD'));
-        setDataConclusao(moment(tarefa.dataConclusao).format('yyyy MM DD'));
+        setDataPrevistaConclusao(moment(tarefa.dataPrevistaConclusao).format('yyyy-MM-DD'));
+        setDataConclusao(moment(tarefa.dataConclusao).format('yyyy-MM-DD'));
         setShowModal(true);
     }
 
@@ -65,58 +66,59 @@ export const Listagem = props => {
                 setMsgErro('Não foi possível atualizar a tarefa, fale com o administrador');
             }
         }
-    }
+    } 
 
     return (
-        <>
-            <div className={"container-listagem " + (tarefas && tarefas.length > 0 ? "" : "vazia")}>
+            <>
+                <div className={"container-listagem " + (tarefas && tarefas.length > 0 ? "" : "vazia")}>
 
-                { tarefas && tarefas.length > 0 ? 
+                    { tarefas && tarefas.length > 0 ? 
 
-                    //se tiver tarefas vai usar o map para passar cada tarefa como props 
-                    //para o componente Item, do contrário exibe imagem sem tarefa
-                    tarefas?.map(tarefa => <Item tarefa={tarefa} key={tarefa.id} selecionarTarefa={selecionarTarefa}/> )
-                    : 
-                    <>
-                        <img src={imgVazio} alt= 'Você não tem tarefas listadas' />
-                        <p>Você ainda não possui tarefas cadastradas!</p>
-                    </>                
-                }            
-            </div>    
-            <Modal show={showModal} onHide={setShowModal(false)} className="container-modal">
-                <Modal.Body>
-                    <p>Alterar uma tarefa</p>
-                    {msgErro && <span className='error'>{msgErro}</span>}
-                    <div className="inputs col-12">
-                        <input type="text" name="nome"
+                        //se tiver tarefas vai usar o map para passar cada tarefa como props 
+                        //para o componente Item, do contrário exibe imagem sem tarefa
+                        tarefas?.map(tarefa => <Item tarefa={tarefa} key={tarefa.id} selecionarTarefa={selecionarTarefa} /> )
+                        : 
+                        <>
+                            <img src={imgVazio} alt= 'Você não tem tarefas listadas' />
+                            <p>Você ainda não possui tarefas cadastradas!</p>
+                        </>                
+                    }            
+                </div>
+                
+
+                <Modal show={showModal} onHide={() => setShowModal(false)} className="container-modal">
+                    <Modal.Body>
+                        <p>Adicionar uma tarefa</p>
+                        {msgErro && <span className='error'>{msgErro}</span>}
+                        <div className="inputs col-12">
+                            <input type="text" name="nome"
                             placeholder="Nome da tarefa" value={nomeTarefa} 
                             onChange={evento => setNomeTarefa(evento.target.value)}/>
 
-                        <input type="text" name="dataPrevistaConclusao"
+                            <input type="text" name="dataPrevistaConclusao"
                             placeholder="Data prevista de conclusão" value={dataPrevistaConclusao}
                             onChange={evento => setDataPrevistaConclusao(evento.target.value)}
                             onFocus={evento => evento.target.type = 'date'}
                             onBlur={evento => dataPrevistaConclusao ? evento.target.type = 'date' : evento.target.type = 'text'}/>
 
-                        <input type="text" name="dataConclusao"
-                            placeholder="Data conclusão" value={dataConclusao}
-                            onChange={evento => setDataConclusao(evento.target.value)} 
+                            <input type="text" name="dataConclusao" 
+                            placeholder="Data Conclusão" value={dataConclusao}
+                            onChange={evento => setDataConclusao(evento.target.value)}
                             onFocus={evento => evento.target.type = 'date'}
-                            onBlur={evento => dataConclusao ? evento.target.type = 'date' : evento.target.type = 'text'} />
-                    </div>                    
-                </Modal.Body>
-                <Modal.Footer>
-                    <button onClick={atualizarTarefa}>Salvar</button>
-                    <button>Excluir Tarefa</button>
-                    <button onClick={() => {
-                        setShowModal(false)
-                        setMsgErro('')
-                        setNomeTarefa('')
-                        setDataPrevistaConclusao('')
-                    }}>Cancelar</button>
+                            onBlur={evento => dataConclusao ? evento.target.type = 'date' : evento.target.type = 'text'}/>
+                        </div>                    
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button onClick={atualizarTarefa}>Salvar</button>
+                        <button onClick={() => {
+                            setShowModal(false)                           
+                        }}>Cancelar</button>
+                    </Modal.Footer>
+                </Modal>  
+                
                     
-                </Modal.Footer>
-            </Modal>
-        </>
+            </>        
+            /*  */
+        
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react';
 import {Modal} from 'react-bootstrap';
 //importando componentes da Home
 import { Filtros } from '../componentes/Filtros';
